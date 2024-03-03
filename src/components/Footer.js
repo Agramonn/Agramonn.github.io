c3dbe1d35829ed298407434315244ec892c89f84
Agramon <pedrogutierrez08@outlook.com>
2024-03-03 02:08:09 -0700

    Updates

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Footer = () => {
-  return <div className="py-4 text-center bg-primary text-white "> &copy; 2024 Pedro Gutierrez all right reserved</div>;
-};
-
-export default Footer;
